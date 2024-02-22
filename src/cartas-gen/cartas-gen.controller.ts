ac0591bfcde267cfdc7e1aeca90f8495a45a6bd7
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2024-02-22 01:46:20 -0300

    Implement creation of generic letters by student ID

--- src/cartas-gen/cartas-gen.controller.ts
+++ src/cartas-gen/cartas-gen.controller.ts
@@ -8,6 +8,20 @@ export class CartasGenController {
   constructor(private readonly cartasGenService: CartasGenService) {}
 
   @Post()
+  createByRut(@Body() createCartasGenDto: CreateCartasGenDto) {
+    /*
+    TODO: Implementar la creación de cartas genéricas por rut
+    -> Se debe buscar el estudiante por su rut
+    -> Se debe buscar la última carta genérica creada para el estudiante, si no existe crear de una con los datos del estudiante enseguida
+    -> Guardar en la base de datos los datos de la carta genérica, sumar uno al count_cg. Si esta ya existía desde antes solo cambiar fecha de actualización y count_cg
+    -> Generar documento dependiendo de lo que sucedió (docx)
+    -> Envío de correo con la carta generada
+    */
+
+    return this.cartasGenService.create(createCartasGenDto);
+  }
+
+  @Post()
   create(@Body() createCartasGenDto: CreateCartasGenDto) {
     return this.cartasGenService.create(createCartasGenDto);
   }
