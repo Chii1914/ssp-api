4f20435a564cd094cb9dca5fb22c150221678a1a
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2024-02-28 14:05:41 -0300

    Add user role to validateOAuthLogin method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,34 @@
 // src/auth/auth.service.ts
-
+import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-
+import { Repository } from 'typeorm';
+import { Alumno } from '../alumno/entities/alumno.entity';
+import { Usuario } from '../usuarios/entities/usuario.entity';
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService) { }
+  constructor(
+  @InjectRepository(Usuario) private usuarioRepository: Repository<Usuario>,
+  @InjectRepository(Alumno) private alumnoRepository: Repository<Alumno>,
+  private jwtService: JwtService) { }
 
-  async validateOAuthLogin(thirdPartyId: string, provider: string, email: string): Promise<string> {
+  async validateOAuthLogin(thirdPartyId: string, provider: string, email: string, rol: string): Promise<string> {
     const payload = {
       sub: thirdPartyId,
       provider,
       email,
-      rol: "admin"
+      rol: rol
     };
     const jwt = this.jwtService.sign(payload);
     return jwt;
+  }
+
+  async findOrCreateUser(email: string) {
+    const admin = await this.usuarioRepository.findOne({ where: { correo: email } });
+    if (admin) {
+      return { email: email, rol: "admin" };
+    }
+    const alumno = await this.alumnoRepository.findOne({ where: { correoInstitucional: email } });
+
   }
 }
